02722a017bf94d2e1dfed54cfebba7fdf9f917ad
rofikHa21 <rofik.hakiki99@gmail.com>
2021-10-17 21:45:07 +0700

    Add : rumus hitung penyustan aktiva tetap

--- Projek/src/com/company/Depresiasi.java
+++ Projek/src/com/company/Depresiasi.java
@@ -16,13 +16,24 @@ public class Depresiasi {
 
         //awal & akhir periode untuk menampilkan reantang waktu pada tabel
 
-//        System.out.print("Awal Periode: ");
-//        Integer awal = data.nextInt();
-//        System.out.print("Akhir Periode: ");
-//        Integer akhir = data.nextInt();
-        String hasil = String.format("%.2f\t%.2f\t%d",aset,residu,periode);
-        System.out.println(hasil);
+        System.out.print("Awal Periode: ");
+        Integer awal = data.nextInt();
+        System.out.print("Akhir Periode: ");
+        Integer akhir = data.nextInt();
 
+        depresiasi(aset, residu, periode, awal, akhir);
+    }
 
+    protected static void depresiasi(Double aset, Double residu, Integer periode, Integer awal, Integer akhir) {
+        if (aset != null && residu != null && periode != null && awal != null && akhir != null) {
+            if (awal < akhir) {
+                System.out.println("Aset\t\tTahun\tAkumulasi\tSisa");
+                System.out.println("==========================================");
+                double penyusutan = (aset - residu) / periode;
+                System.out.println(penyusutan);
+            } else {
+                System.out.println("Pastikan Nilai Awal Lebih Kecil");
+            }
+        }
     }
 }

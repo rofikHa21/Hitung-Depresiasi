2a45299754c78ce695aeb5fffe9ba39e3f502f6c
rofikHa21 <rofik.hakiki99@gmail.com>
2021-10-17 21:27:40 +0700

    Add : Menambah 5 variabel input

--- Projek/src/com/company/Depresiasi.java
+++ Projek/src/com/company/Depresiasi.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 public class Depresiasi {
 
     public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+
+        System.out.println("Program Menghitung Depresiasi Aktiva Tetap");
+        System.out.print("===========================================\n\nNominal Perolehan Aset : ");
+        Double aset = data.nextDouble();
+        System.out.print("Nilai Residu : ");
+        Double residu = data.nextDouble();
+        System.out.print("Periode Depresiasi: ");
+        Integer periode = data.nextInt();
+
+        //awal & akhir periode untuk menampilkan reantang waktu pada tabel
+
+//        System.out.print("Awal Periode: ");
+//        Integer awal = data.nextInt();
+//        System.out.print("Akhir Periode: ");
+//        Integer akhir = data.nextInt();
+        String hasil = String.format("%.2f\t%.2f\t%d",aset,residu,periode);
+        System.out.println(hasil);
+
 
     }
 }
